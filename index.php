539485a4e28ea37e0d40ae359ce5d8ea9e194ba3
xdpirate <1757462+xdpirate@users.noreply.github.com>
2023-06-11 17:07:51 +0200

    `

--- index.php
+++ index.php
@@ -43,24 +43,24 @@ if(!$link) {
 mysqli_query($link, "CREATE DATABASE IF NOT EXISTS gamehorizon");
 mysqli_select_db($link, "gamehorizon");
 mysqli_query($link, "
-    CREATE TABLE IF NOT EXISTS gamesUnreleased (
-        ID int NOT NULL AUTO_INCREMENT,
-        GameName varchar(255) NOT NULL,
-        ReleaseDate varchar(255) NOT NULL,
-        Platforms varchar(255) NOT NULL,
-        PRIMARY KEY (ID)
+    CREATE TABLE IF NOT EXISTS `gamesUnreleased` (
+        `ID` int NOT NULL AUTO_INCREMENT,
+        `GameName` varchar(255) NOT NULL,
+        `ReleaseDate` varchar(255) NOT NULL,
+        `Platforms` varchar(255) NOT NULL,
+        PRIMARY KEY (`ID`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;");
 
 mysqli_query($link, "
-    CREATE TABLE IF NOT EXISTS gamesReleased (
-        ID int NOT NULL AUTO_INCREMENT,
-        GameName varchar(255) NOT NULL,
-        Platforms varchar(255) NOT NULL,
-        PRIMARY KEY (ID)
+    CREATE TABLE IF NOT EXISTS `gamesReleased` (
+        `ID` int NOT NULL AUTO_INCREMENT,
+        `GameName` varchar(255) NOT NULL,
+        `Platforms` varchar(255) NOT NULL,
+        PRIMARY KEY (`ID`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;");
 
-mysqli_query($link, "CREATE TABLE IF NOT EXISTS gamesTBA LIKE gamesReleased;");
-mysqli_query($link, "CREATE TABLE IF NOT EXISTS gamesCollected LIKE gamesReleased;");
+mysqli_query($link, "CREATE TABLE IF NOT EXISTS `gamesTBA` LIKE `gamesReleased`;");
+mysqli_query($link, "CREATE TABLE IF NOT EXISTS `gamesCollected` LIKE `gamesReleased`;");
 
 if(isset($_GET['delete']) && isset($_GET['from'])) {
     $ID = mysqli_real_escape_string($link, $_GET['delete']);
