0e07a69682ebc1c3d6f7db5c8aa176c956a9d5dd
xdpirate <1757462+xdpirate@users.noreply.github.com>
2024-03-12 02:20:42 +0100

    Invalidate cache upon update

--- index.php
+++ index.php
@@ -29,6 +29,15 @@ include("./credentials.php");
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 ini_set("display_errors", 1);
 
+if(isset($_GET['invalidateCache'])) {
+    header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+    header("Location: ./");
+}
+
 $platformList = ["PS5", "PS4", "XSX", "XB1", "Switch", "PC", "Android", "PSVR2", "iOS"];
 sort($platformList);
 
